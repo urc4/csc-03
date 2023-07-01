8f099c21de2d7c0801c1ffa5d3b191f8fbc34ee4
urc4 <121357043+urc4@users.noreply.github.com>
2023-07-01 20:14:00 -0300

    Display phone default layout

--- App/js/main.js
+++ App/js/main.js
@@ -8,6 +8,84 @@ function showScreen(screenId) {
   targetScreen.classList.add("active");
 }
 
-// Menu
+// Display current hours
+let currentDate = new Date();
+let currentHour = currentDate.getHours();
+let currentMinutes = currentDate.getMinutes();
+let formattedTime =
+  currentHour.toString().padStart(2, "0") +
+  ":" +
+  currentMinutes.toString().padStart(2, "0");
 
-// Schedule
+document.getElementById("current-hours").textContent = formattedTime;
+
+function updateTime() {
+  let timeDisplay = document.getElementById("current-hours");
+
+  let timeInterval = setInterval(function () {
+    currentDate = new Date();
+    currentHour = currentDate.getHours();
+    currentMinutes = currentDate.getMinutes();
+    formattedTime =
+      currentHour.toString().padStart(2, "0") +
+      ":" +
+      currentMinutes.toString().padStart(2, "0");
+    timeDisplay.textContent = formattedTime;
+  }, 60 * 1000);
+}
+
+updateTime();
+
+// Display battery life
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function updateBatteryLevel(batteryLevel) {
+  let icon = document.getElementById("battery-icon");
+
+  // Update the battery level every 5 minutes
+  let batteryInterval = setInterval(function () {
+    // Decrease the battery level
+    batteryLevel -= 1;
+    if (batteryLevel < 0) {
+      batteryLevel = 1;
+    }
+    document.getElementById("battery-life").textContent = `${batteryLevel} %`;
+
+    // Update the icon based on the battery level
+    if (batteryLevel >= 80) {
+      icon.textContent = "battery_full";
+    } else if (batteryLevel >= 20) {
+      icon.textContent = "battery_std";
+    } else if (batteryLevel < 20) {
+      icon.textContent = "battery_alert";
+    } else if (batteryLevel > 0) {
+      icon.textContent = "battery_alert";
+    } else {
+      clearInterval(batteryInterval);
+      icon.textContent = "battery_alert";
+    }
+  }, 5 * 60 * 1000);
+}
+
+let batteryLevel = getRandomInt(0, 100);
+document.getElementById("battery-life").textContent = `${batteryLevel} %`;
+let icon = document.getElementById("battery-icon");
+
+if (batteryLevel >= 80) {
+  icon.textContent = "battery_full";
+} else if (batteryLevel >= 60) {
+  icon.textContent = "battery_std";
+} else if (batteryLevel >= 20) {
+  icon.textContent = "battery_std";
+} else if (batteryLevel > 0) {
+  icon.textContent = "battery_alert";
+} else {
+  clearInterval(batteryInterval);
+  icon.textContent = "battery_alert";
+}
+
+updateBatteryLevel(batteryLevel);
